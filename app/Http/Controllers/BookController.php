625e5f1ea17e262ac16695fecda99bd0d96e1339
stefan Åhlander <cs.ahlander@gmail.com>
2020-01-28 10:31:55 +0100

    Change search

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -15,6 +15,14 @@ class BookController extends Controller
         $author = request('author');
         $title = request('title');
 
+        if(is_null($author) && is_null($title)) {
+
+        } else {
+            $author = (!is_null($author)) ? $author : 'string that for sure is not in the books-table';
+            $title = (!is_null($title)) ? $title : 'string that for sure is not in the books-table';
+        }
+
+
         $bookModel = new BookModel();
  
         return view("search-results", ['books' => $bookModel->find($author, $title), 'cart' => $this->getCart()]);
